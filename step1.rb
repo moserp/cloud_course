f9b37ff0c2d394f57f6cd2ba00063dc1a562ea6d
Paul Moser <paul.t.moser@gmail.com>
2011-01-07 15:09:54 +0000

    Reorganise order of installation steps

--- step1.rb
+++ step1.rb
@@ -4,14 +4,13 @@ policy :ubuntu, :roles => :app do
   requires :mtu
   requires :apt_sources
 
-  requires :apache
   requires :app
 end
 
 deployment do
   delivery :capistrano do
     set :user, 'root'
-    role :app, '109.144.14.45', :primary => true
+    role :app, '109.144.14.42', :primary => true
     default_run_options[:pty] = true
   end
 end
